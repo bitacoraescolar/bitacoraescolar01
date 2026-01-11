e4ff6ac5e3437f0764f4ce0a35767e5ea5d2ae96
bitacoraescolar <bitacoraescolar.cl@gmail.com>
2026-01-11 11:17:43 -0300

    new js

--- script.js
+++ script.js
@@ -132,6 +132,27 @@ function createCardHTML(event) {
     `;
 }
 
+function openTab(evt, tabName) {
+    // Declarar variables
+    var i, tabcontent, tablinks;
+
+    // Ocultar todos los elementos con clase="tab-content"
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Quitar la clase "active" de todos los botones
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Mostrar la pestaña actual y añadir clase "active" al botón
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 // Run on load
 
 document.addEventListener('DOMContentLoaded', fetchSchoolEvents);
